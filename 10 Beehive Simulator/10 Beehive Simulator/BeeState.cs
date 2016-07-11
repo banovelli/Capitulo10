b1dad23f6fe2bc6f72a9123e337603add6a56156
banovelli <bnovelli@gmail.com>
2016-07-11 11:05:23 -0300

    add solutions

---
+++ 10 Beehive Simulator/10 Beehive Simulator/BeeState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _0_Beehive_Simulator
+{
+    public enum BeeState
+    {
+        Idle,
+        FlyingToFlower,
+        GatheringNectar,
+        ReturningToHive,
+        MakingHoney,
+        Retired,
+    }
+
+}
---
+++ 10 Beehive Simulator/10 Beehive Simulator/Form1.designer.cs
@@ -0,0 +1,342 @@
+﻿namespace _0_Beehive_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.startSimulation = new System.Windows.Forms.ToolStripButton();
+            this.reset = new System.Windows.Forms.ToolStripButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HoneyInHive = new System.Windows.Forms.Label();
+            this.Flowers = new System.Windows.Forms.Label();
+            this.NectarInFlowers = new System.Windows.Forms.Label();
+            this.Bees = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FramesRun = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FrameRate = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.printToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startSimulation,
+            this.reset,
+            this.toolStripSeparator,
+            this.openToolStripButton,
+            this.saveToolStripButton,
+            this.printToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(292, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // startSimulation
+            // 
+            this.startSimulation.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.startSimulation.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.startSimulation.Name = "startSimulation";
+            this.startSimulation.Size = new System.Drawing.Size(112, 22);
+            this.startSimulation.Text = "Start Simulation";
+            this.startSimulation.Click += new System.EventHandler(this.startSimulation_Click);
+            // 
+            // reset
+            // 
+            this.reset.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.reset.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(49, 22);
+            this.reset.Text = "Reset";
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.56165F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.43835F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.HoneyInHive, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Flowers, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.NectarInFlowers, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Bees, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FramesRun, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.FrameRate, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(292, 110);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "# Flowers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total honey in the hive";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total nectar in the field";
+            // 
+            // HoneyInHive
+            // 
+            this.HoneyInHive.AutoSize = true;
+            this.HoneyInHive.Location = new System.Drawing.Point(174, 34);
+            this.HoneyInHive.Name = "HoneyInHive";
+            this.HoneyInHive.Size = new System.Drawing.Size(88, 17);
+            this.HoneyInHive.TabIndex = 9;
+            this.HoneyInHive.Text = "HoneyInHive";
+            // 
+            // Flowers
+            // 
+            this.Flowers.AutoSize = true;
+            this.Flowers.Location = new System.Drawing.Point(174, 17);
+            this.Flowers.Name = "Flowers";
+            this.Flowers.Size = new System.Drawing.Size(56, 17);
+            this.Flowers.TabIndex = 11;
+            this.Flowers.Text = "Flowers";
+            // 
+            // NectarInFlowers
+            // 
+            this.NectarInFlowers.AutoSize = true;
+            this.NectarInFlowers.Location = new System.Drawing.Point(174, 51);
+            this.NectarInFlowers.Name = "NectarInFlowers";
+            this.NectarInFlowers.Size = new System.Drawing.Size(109, 17);
+            this.NectarInFlowers.TabIndex = 6;
+            this.NectarInFlowers.Text = "NectarInFlowers";
+            // 
+            // Bees
+            // 
+            this.Bees.AutoSize = true;
+            this.Bees.Location = new System.Drawing.Point(174, 0);
+            this.Bees.Name = "Bees";
+            this.Bees.Size = new System.Drawing.Size(40, 17);
+            this.Bees.TabIndex = 10;
+            this.Bees.Text = "Bees";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# Bees";
+            // 
+            // FramesRun
+            // 
+            this.FramesRun.AutoSize = true;
+            this.FramesRun.Location = new System.Drawing.Point(174, 68);
+            this.FramesRun.Name = "FramesRun";
+            this.FramesRun.Size = new System.Drawing.Size(81, 17);
+            this.FramesRun.TabIndex = 8;
+            this.FramesRun.Text = "FramesRun";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Frames run";
+            // 
+            // FrameRate
+            // 
+            this.FrameRate.AutoSize = true;
+            this.FrameRate.Location = new System.Drawing.Point(174, 85);
+            this.FrameRate.Name = "FrameRate";
+            this.FrameRate.Size = new System.Drawing.Size(78, 17);
+            this.FrameRate.TabIndex = 7;
+            this.FrameRate.Text = "FrameRate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Frame rate";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(124, 18);
+            this.toolStripStatusLabel1.Text = "Simulation paused";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 244);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(292, 23);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(5, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 116);
+            this.listBox1.TabIndex = 8;
+            // 
+            // openToolStripButton
+            // 
+            this.openToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+            this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripButton.Name = "openToolStripButton";
+            this.openToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.openToolStripButton.Text = "&Open";
+            this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.saveToolStripButton.Text = "&Save";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // printToolStripButton
+            // 
+            this.printToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.printToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("printToolStripButton.Image")));
+            this.printToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printToolStripButton.Name = "printToolStripButton";
+            this.printToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.printToolStripButton.Text = "&Print";
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 267);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton startSimulation;
+        private System.Windows.Forms.ToolStripButton reset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label HoneyInHive;
+        private System.Windows.Forms.Label Flowers;
+        private System.Windows.Forms.Label NectarInFlowers;
+        private System.Windows.Forms.Label Bees;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FramesRun;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label FrameRate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.ToolStripButton printToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+    }
+}
+
